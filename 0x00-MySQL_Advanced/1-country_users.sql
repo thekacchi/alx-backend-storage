70f33bdd45ffb9804b516c69c7ed4051ef3e50b8
Onyedikachi Eze <thekacchi1@gmail.com>
2024-05-11 12:06:27 +0000

    user tsblr eith country column

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,9 @@
 -- SQL script that creates a table 'users' following:
--- id (integer), email (string, 255), name (string, 255)
+-- id (integer), email (string, 255), name (string, 255), country
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
