f98fffe8737dcc9160712eae5b76c6f828e2f49b
Onyedikachi Eze <thekacchi1@gmail.com>
2024-05-11 12:50:13 +0000

    removed delim

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,7 @@
 -- script that creates a trigger
--- that decreases the quantity of an item after adding a new order.iDELIMITER //
+-- that decreases the quantity of an item after adding a new order
+
+DELIMITER //
 
 CREATE TRIGGER update_quantity AFTER INSERT ON orders
 FOR EACH ROW
