ffbe307c04146ea25c5bf430e30314f865250f93
Onyedikachi Eze <thekacchi1@gmail.com>
2024-05-11 12:46:17 +0000

    update

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,14 @@
+-- script that creates a trigger
+-- that decreases the quantity of an item after adding a new order.iDELIMITER //
+
+CREATE TRIGGER update_quantity AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+//
+
+DELIMITER ;
+
